11ac525c5fb5e447dc7bee85fb1d763b82f3890e
Michel <106674127+MichelZero@users.noreply.github.com>
2022-06-29 15:09:18 -0300

    Create ex03.py

---
+++ ex03.py
@@ -0,0 +1,11 @@
+#
+# Autores:
+# Michel Silva
+# Emanuel Franklyn
+# Carlos Eduardo
+#
+# data: 29/06/2022
+#
+# 3 – dado uma lista de valores, verifique quanto vezes cada elemento se repete (use conjuntos).
+# l1 = ['a','b','b','a','c','c','c','d','d',’e']
+
