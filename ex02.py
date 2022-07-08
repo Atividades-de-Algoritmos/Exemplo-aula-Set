3be7d14297edb726e1557cf3ce860f200f74e9e9
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-08 16:54:07 -0300

    Maior e menor valor com set() ⬆⬇

--- ex02.py
+++ ex02.py
@@ -4,24 +4,22 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 29/06/2022
+# data: 08/07/2022
 #
 # 2- ler um conjunto de 5 números e imprimir o menor e maior valor.
 
+# Entrada e processamento de dados
 
-###          1         #####################################
-c3 = set() # criar um conjunto vazio para c3
+c3 = set() # Criando um conjunto vazio para c3
 
-#entrada de dados
-for i in range(5): #entrada de dados do conjunto c3 com 5 valores
-  valor = int(input("informe um valor: ")) #entrada de dados do valor do conjunto c3
-  c3.add(valor) #adicionar o valor ao conjunto c3 com 4 valores
-media = sum(c3) / len(c3) #calcular a média do conjunto c3 com 5 valores
+for i in range(5): # For no range(5) significa que vai executar 5 vezes
+  valor = int(input("Informe um valor: ")) # Solicitando um valor inteiro do usuário
+  c3.add(valor) # Adicionando o valor ao conjunto c3
 
-menor = min(c3) #calcular o menor valor do conjunto c3 com 5 valores e armazenar na variável menor
-maior = max(c3) #calcular o maior valor do conjunto c3 com 5 valores e armazenar na variável maior
+menor = min(c3) # Calculando o menor valor do conjunto c3 com min() e armazenando na variável menor
+maior = max(c3) # Calculando o maior valor do conjunto c3 com max() e armazenando na variável maior
 
-#saída de dados
-print(f"o conjunto {c3}, a média dos valores é {media}") #mostrar o conjunto c3 com os valores e a média
-print(f"o conjunto {c3}, a média dos valores é {media:.2f}") # para formatar a saída valor:.2f = 2 casas decimais
-print(f"o menor é {menor} e maior valores é {maior}") #mostrar o menor e maior valor do conjunto c3 com 5 valores
+# Saída de dados
+
+print(f"\nO menor é {menor} e o maior valor é {maior}") # Imprimindo o menor e maior valor do conjunto c3 com 5 valores
+print('\nfim do programa')
