281844968a0ce22811010f2ab181a0d57c73305b
Carlos <lopes.carlos@academico.ifpb.edu.br>
2022-07-08 16:54:46 -0300

    Analisando Ocorrências 📋

--- ex03.py
+++ ex03.py
@@ -4,22 +4,25 @@
 # Emanuel Franklyn
 # Carlos Eduardo
 #
-# data: 29/06/2022
+# data: 08/07/2022
 #
 # 3 – dado uma lista de valores, verifique quanto vezes cada elemento se repete (use conjuntos).
 # l1 = ['a','b','b','a','c','c','c','d','d','e']
 
-# criar a lista l1
-l1 = ['a','b','b','a','c','c','c','d','d','e']
-print(l1) #mostrar a lista l1
 
-# cats l1 -> set
-conjunto = set(l1) #criar um conjunto cats com os valores da lista l1 (set)
-print(conjunto) #mostrar o conjunto cats com os valores
+# Entrada de dados
 
-# processamento e saída de dados
-for i in conjunto: #para cada valor do conjunto cats
-  print(f"o valor {i} se repete {l1.count(i)} vezes") #mostrar o valor e a quantidade de vezes que ele se repete na lista l1
-  print(f"o valor {i} se repete {l1.count(i):.2f} vezes") # para formatar a saída valor:.2f = 2 casas decimais (:.2f)
+l1 = ['a','b','b','a','c','c','c','d','d','e'] # Criando a lista l1
+print(f'lista l1 -> {l1}\n') # Imprimindo a lista l1
 
+# Cast l1 -> set
+conjunto = set(l1) # Criando um conjunto com os valores da lista l1 (set)
+print(f'Set l1 -> {conjunto}\n') # Imprimindo o conjunto com os valores
 
+# Processamento e saída de dados
+
+for i in conjunto: # Para cada valor do conjunto
+  print(f"O valor {i} se repete {l1.count(i)} vezes") # Mostrando o valor e a quantidade de vezes que ele se repete na lista l1
+  # print(f"O valor {i} se repete {l1.count(i):.2f} vezes") # Para formatar a saída valor:.2f = 2 casas decimais (:.2f)
+
+print('\nfim do programa')
